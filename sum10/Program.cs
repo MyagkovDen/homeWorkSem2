8ee3da5f340197b3ca8f5e8121757b728eb14a50
Denis Myagkov <tomich-84@mail.ru>
2022-11-18 01:07:35 +0300

    sum10 changed

--- sum10/Program.cs
+++ sum10/Program.cs
@@ -7,9 +7,23 @@
 */
 
 Console.Write("Введите трехзначное число: ");
-int number = int.Parse(Console.ReadLine());
+string userInput = Console.ReadLine() ??"";
 
-int a = number / 10;
-int b = a % 10;
-
-Console.WriteLine($"{number} -> {b}");
+int number;
+try
+{
+    number = int.Parse(userInput);
+}
+catch (System.Exception ex)
+{
+    Console.WriteLine($"Ошибка ввода! {ex.Message}");
+    return;
+}
+if (99 < number && number < 1000)
+{
+    int a = number / 10;
+    int b = a % 10;
+    Console.WriteLine($"{number} -> {b}");
+}
+else
+Console.WriteLine("Ошибка ввода! Введенное число не является трехзначным");
